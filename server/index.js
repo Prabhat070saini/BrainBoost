99dd1c612cb499a6707ee21eb563215b476dd02d
Prabhat <Prabhatsaini070@gmail.com>
2023-07-26 14:35:56 +0530

    cors

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser())
 app.use(
     cors({
-        origin: 'https://brainboost-tkgp.onrender.com',
+        origin: 'https://brain-boost-xi.vercel.app',
         credential: true,
     })
 );
